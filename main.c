266f6d188db2edd29ebfe65b678c0e61dfefe101
Osam-Frimpong Schandorf <osamfrimpong@yahoo.com>
2023-09-21 19:35:57 +0000

    did some refactoring

--- main.c
+++ main.c
@@ -1,12 +1,10 @@
 #include "monty.h"
 
-osam_monty_global_t osam_global_var;
-
 /**
  * main - Entry point
  *
  * @argc: argument count
- * @argv: argument vector
+ * @argv: string arguments vector
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
@@ -15,7 +13,7 @@ int main(int argc, char *argv[])
 	FILE *file_desc;
 	size_t size = 256;
 	ssize_t num_lines = 0;
-	char *lines[2] = {NULL, NULL};
+	char *splitted_lines[2] = {NULL, NULL};
     char *message;
 
 	file_desc = osam_check_input(argc, argv);
@@ -23,14 +21,14 @@ int main(int argc, char *argv[])
 	num_lines = getline(&osam_global_var.text_buffer, &size, file_desc);
 	while (num_lines != -1)
 	{
-		lines[0] = strtok(osam_global_var.text_buffer, " \t\n");
-		if (lines[0] && lines[0][0] != '#')
+		splitted_lines[0] = strtok(osam_global_var.text_buffer, " \t\n");
+		if (splitted_lines[0] && splitted_lines[0][0] != '#')
 		{
-			f = find_osam_opcode(lines[0]);
+			f = find_osam_opcode(splitted_lines[0]);
 			if (!f)
 			{
                 message = malloc(sizeof(char) * OSAM_MAX_BUFFER_SIZE);
-                snprintf(message, OSAM_MAX_BUFFER_SIZE, "L%u: unknown instruction %s\n", osam_global_var.curr_line,lines[0]);
+                snprintf(message, OSAM_MAX_BUFFER_SIZE, "L%u: unknown instruction %s\n", osam_global_var.curr_line,splitted_lines[0]);
 		        osam_print_out(message, 2);
                 free(message);
 				osam_free_global_var(osam_global_var);
--- osam_linked_list_helpers.c
+++ osam_linked_list_helpers.c
@@ -8,32 +8,32 @@
  */
 stack_t *osam_add_node_end(stack_t **head, const int n)
 {
-	stack_t *temp, *aux;
+	stack_t *temporary_node, *aux;
 
 	if (head == NULL)
 		return (NULL);
-	temp = malloc(sizeof(stack_t));
-	if (!temp)
+	temporary_node = malloc(sizeof(stack_t));
+	if (!temporary_node)
 	{
 		osam_print_out("Error: malloc failed\n", 2);
 		osam_free_global_var(osam_global_var);
 		exit(EXIT_FAILURE);
 	}
-	temp->n = n;
-	/*Careful with the first time*/
+	temporary_node->n = n;
+	
 	if (*head == NULL)
 	{
-		temp->next = *head;
-		temp->prev = NULL;
-		*head = temp;
+		temporary_node->next = *head;
+		temporary_node->prev = NULL;
+		*head = temporary_node;
 		return (*head);
 	}
 	aux = *head;
 	while (aux->next)
 		aux = aux->next;
-	temp->next = aux->next;
-	temp->prev = aux;
-	aux->next = temp;
+	temporary_node->next = aux->next;
+	temporary_node->prev = aux;
+	aux->next = temporary_node;
 	return (aux->next);
 }
 
@@ -45,30 +45,30 @@ stack_t *osam_add_node_end(stack_t **head, const int n)
  */
 stack_t *osam_add_node_start(stack_t **head, const int n)
 {
-	stack_t *temp;
+	stack_t *temporary_node;
 
 	if (head == NULL)
 		return (NULL);
-	temp = malloc(sizeof(stack_t));
-	if (!temp)
+	temporary_node = malloc(sizeof(stack_t));
+	if (!temporary_node)
 	{
 		osam_print_out("Error: malloc failed\n", 2);
 		osam_free_global_var(osam_global_var);
 		exit(EXIT_FAILURE);
 	}
-	temp->n = n;
+	temporary_node->n = n;
 	/*Careful with the first time*/
 	if (*head == NULL)
 	{
-		temp->next = *head;
-		temp->prev = NULL;
-		*head = temp;
+		temporary_node->next = *head;
+		temporary_node->prev = NULL;
+		*head = temporary_node;
 		return (*head);
 	}
-	(*head)->prev = temp;
-	temp->next = (*head);
-	temp->prev = NULL;
-	*head = temp;
+	(*head)->prev = temporary_node;
+	temporary_node->next = (*head);
+	temporary_node->prev = NULL;
+	*head = temporary_node;
 	return (*head);
 }
 
